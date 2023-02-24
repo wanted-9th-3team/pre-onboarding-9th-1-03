b1281e57a338fa918ef465162d7e583fb5b38ac0
skyhanull <anky0425@naver.com>
2023-02-24 19:57:50 +0900

    feat: 투두페이지 추가

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Todo from '../components/Todo'
 
 function TodoPage() {
-  return <div>Todo</div>
+  return (
+    <>
+      <Todo />
+    </>
+  )
 }
 
 export default TodoPage
