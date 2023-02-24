4ba8a15330016453a5514730eaa83740c51813de
skyhanull <anky0425@naver.com>
2023-02-24 20:19:18 +0900

    refactor: react.fragment삭제

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import Todo from '../components/Todo'
 
 function TodoPage() {
-  return (
-    <>
-      <Todo />
-    </>
-  )
+  return <Todo />
 }
 
 export default TodoPage
